58fa02920b310adb63d37c1bd74e3a3b15640f0d
Alexander Fortowski <a.fortowski@gmail.com>
2017-05-15 14:57:11 +0200

    Koding modified

--- Sammelsurium/AD-Move-LDAP.py
+++ Sammelsurium/AD-Move-LDAP.py
@@ -2,7 +2,7 @@
 import win32com.client
 
 dso = win32com.client.GetObject("LDAP:")
-obj = dso.OpenDSObject("LDAP://DC=de/DC=funkegrp/OU=подразделение куда","домен\\пользователь", "пароль", 0)
+obj = dso.OpenDSObject("LDAP://DC=de/DC=funkegrp/OU=podrazdelenie kuda","domen\\polzovatel", "parol", 0)
 
 
-#obj.MoveHere("LDAP://DC=ru/DC=домен/OU=подразделение куда/CN=_CoolAdmin", "CN=_CoolAdmin")
+#obj.MoveHere("LDAP://DC=ru/DC=domen/OU=podrazdelenie kuda/CN=_CoolAdmin", "CN=_CoolAdmin")
